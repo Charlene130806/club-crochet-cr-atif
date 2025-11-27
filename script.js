019130b830e698c61e62409385deca9ad366b9dc
Charlene130806 <c.evrard@ileps.fr>
2025-11-27 15:20:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// Navigation
 document.querySelectorAll(".nav-btn").forEach(btn => {
     btn.addEventListener("click", () => {
-        const target = btn.dataset.target;
+        const target = btn.getAttribute("data-target");
         document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
         document.getElementById(target).classList.add("active");
     });
 });
 
+// Boutons Home
 document.querySelectorAll(".home-btn").forEach(btn => {
     btn.addEventListener("click", () => {
         document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
